2f5a2375d13eaa95a223f871936741b942ce667d
SuryaprakashSingh25 <singh.suraj1025@gmail.com>
2025-04-05 13:59:11 +0530

    Product API Setup and Tests Containerization

---
+++ product-service/src/main/java/com/microservices/product/service/ProductService.java
@@ -0,0 +1,37 @@
+package com.microservices.product.service;
+
+import com.microservices.product.dto.ProductRequest;
+import com.microservices.product.dto.ProductResponse;
+import com.microservices.product.model.Product;
+import com.microservices.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductResponse createProduct(ProductRequest productRequest){
+        Product product = Product.builder()
+                .name(productRequest.name())
+                .description(productRequest.description())
+                .price(productRequest.price())
+                .build();
+        productRepository.save(product);
+        log.info("Product Created Successfully");
+        return new ProductResponse(product.getId(),product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(),product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
+    }
+}
